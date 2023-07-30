c716683b82443d5f46a94f303073da115beecd2e
tasnim <tasnim0tantawi@gmail.com>
2023-07-30 18:46:49 +0300

    added flowbite

---
+++ src/components/Avatar.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Avatar() {
+    return (<img className="w-10 h-10 p-1 rounded-full ring-2 ring-gray-300 dark:ring-gray-500"
+             src="/docs/images/people/profile-picture-5.jpg" alt="Bordered avatar"/>);
+
+}
+
+export default Avatar;
